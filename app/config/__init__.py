932dded366a4009c0070aadccc4f7c72fb9d1745
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2025-05-10 00:28:25 +0300

    add permission to dependencies.py

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -3,5 +3,6 @@ from .dependencies import (
     get_settings,
     get_jwt_auth_manager,
     get_accounts_email_notificator,
-    get_s3_storage_client
+    get_s3_storage_client,
+    require_moderator_or_admin_user
 )
