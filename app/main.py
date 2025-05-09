29fbd70fc38053f235c139b4bdb6ed15edcd340b
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2025-05-10 00:28:48 +0300

    add routes for movies

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.routes import accounts, profiles
+from app.routes import accounts, profiles, movies
 
 app = FastAPI()
 
 app.include_router(accounts, prefix="/accounts", tags=["accounts"])
 app.include_router(profiles, prefix=f"/profiles", tags=["profiles"])
+app.include_router(movies, prefix="/movies", tags=["movies"])
